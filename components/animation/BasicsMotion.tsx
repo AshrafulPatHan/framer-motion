e350537e1c6957044db360358a56e7b5e46281b5
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-01 19:44:20 +0600

    add dox animation

--- components/animation/BasicsMotion.tsx
+++ components/animation/BasicsMotion.tsx
@@ -1,11 +1,25 @@
+import * as motion from "motion/react-client"
 
 
 export function BasicsMotion() {
     return(
-        <div >
-            <div className="flex justify-center items-center h-screen w-screen ">
-                framer motion
-            </div> 
+        <div className="flex justify-center items-center h-screen w-screen ">
+            <motion.div
+            className="w-[120px] h-[120px] bg-amber-200 p-6 "
+            initial={{
+                rotate: "0deg",
+            }}
+            animate={{
+                rotate: "360deg",
+            }}
+            transition={{
+                duration:2,
+                // type:"spring",
+                ease:"backInOut"
+            }}
+            >
+                <p className="text-center mt-5">motion</p>
+            </motion.div> 
         </div>
     );
 };
