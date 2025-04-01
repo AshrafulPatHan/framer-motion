d159a617280a61eb0c56c4b7340f82f6b01b4b33
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-01 18:09:49 +0600

    add

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center ">
-      <Link href="/first-motion" >First</Link>
+      <Link href="/first-motion" >First-Practice</Link>
     </div>
   );
 }
