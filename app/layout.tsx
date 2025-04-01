63abaf2b0a08f9ed8c5bcb4cd287b4dad6590545
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-01 16:10:31 +0600

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Admin-Panel",
+  title: "Framer-Motion",
   description: "Admin panel of ashraful pathan",
 };
 
