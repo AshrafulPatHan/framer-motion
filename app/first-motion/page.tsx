dbb9796f3ba8eb79ef21859b9ca3266e219cdf16
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-01 17:13:58 +0600

    fix error

---
+++ app/first-motion/page.tsx
@@ -0,0 +1,12 @@
+
+export function firstMotion() {
+    return(
+        <div>
+            h
+        </div>
+    )
+}
+
+
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <div>
-      Hello world
+    <div className="flex flex-col items-center justify-center ">
+      <Link href="/first-motion" >First</Link>
     </div>
   );
 }
