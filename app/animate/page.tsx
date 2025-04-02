ac21ad9afb22f1c551c6bb472145a3365f296902
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-02 20:19:16 +0600

    add 3d bootol

--- app/animate/page.tsx
+++ app/animate/page.tsx
@@ -2,13 +2,15 @@ import Spline from '@splinetool/react-spline/next';
 
 export default function animate() {
   return (
-    <main >
-      <Spline
+    <main 
+    className="flex items-center justify-center min-h-screen bg-gray-100"
+    >
+      <div className="w-[90vw] h-[50vh] md:w-[60vw] md:h-[70vh]">
+        <Spline
         scene="https://prod.spline.design/tCxjsVqYpW2RQ4mf/scene.splinecode" 
-        className='w-[50vw] '
-      />
-      {/* <script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.82/build/spline-viewer.js"></script>
-      <spline-viewer url="https://prod.spline.design/tCxjsVqYpW2RQ4mf/scene.splinecode"></spline-viewer> */}
+        />
+      </div>
+      
     </main>
   );
 }
