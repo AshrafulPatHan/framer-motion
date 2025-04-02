38224fbe12339e27e26bb6ca780c3f810084d6e8
AshrafulPatHan <ashrafulpathan3927@gmail.com>
2025-04-02 20:00:26 +0600

    3d animation is add

--- app/animate/page.tsx
+++ app/animate/page.tsx
@@ -1,11 +1,14 @@
-
-
+import Spline from '@splinetool/react-spline/next';
 
 export default function animate() {
-  return(
-    <div>
-      a
-    </div>
-  )
+  return (
+    <main >
+      <Spline
+        scene="https://prod.spline.design/tCxjsVqYpW2RQ4mf/scene.splinecode" 
+        className='w-[50vw] '
+      />
+      {/* <script type="module" src="https://unpkg.com/@splinetool/viewer@1.9.82/build/spline-viewer.js"></script>
+      <spline-viewer url="https://prod.spline.design/tCxjsVqYpW2RQ4mf/scene.splinecode"></spline-viewer> */}
+    </main>
+  );
 }
-
